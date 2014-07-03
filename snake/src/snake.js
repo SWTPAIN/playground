311007dbad532869f19ff931939db60441114e15
KaFaiC <choikafai2004@gmail.com>
2014-07-03 09:40:01 +0800

    Added logic of losing game by contact the snake body

--- snake/src/snake.js
+++ snake/src/snake.js
@@ -3,31 +3,50 @@ $(document).ready(function(){
 })
 
 $(document).keydown(function(e){
-  if(e.keyCode == 37){
+  if(e.keyCode == 37 && snake.direction != 'r'){
     snake.direction = 'l';
-  }else if(e.keyCode == 38){
+  }else if(e.keyCode == 38 && snake.direction != 'd'){
     snake.direction = 'u';
-  }else if(e.keyCode == 39){
+  }else if(e.keyCode == 39 && snake.direction != 'l'){
     snake.direction = 'r';
-  }else if(e.keyCode == 40){
+  }else if(e.keyCode == 40 && snake.direction != 'u'){
     snake.direction = 'd';
+  }else if(e.keyCode == 32 && isPaused == false){
+    isPaused = true;
+  }else if(e.keyCode == 32 && isPaused == true){
+    isPaused = false;
   }
 })
 
 function game_init(){
   var board_size = 40;
   var speed = 200;
+  console.log(speed);
+  isPaused = true;  
+  renderController();
   renderBoard(board_size);
   generateSnake(board_size);
   generateFruit(board_size);
-  // setTimeout(function(){gameUpdate()}, speed);
-  setInterval(function(){move(board_size)}, speed);
+  setInterval(function(){
+    if (!isPaused) {
+    move(board_size);
+    } 
+  },speed);
+  // setInterval(function(){move(board_size)}, speed);
 
 }
 
 
+function renderController(){
+  $('body').html('<div id="controller">'
+    +'<h1>Snake Game</h1>'
+    +'<p>You may start by pressing space key</p>'
+    +'</div>'
+  )  
+}
+
 function renderBoard(size){
-  $('body').html('<div id="board"></div>')
+  $('body').append('<div id="board"></div>')
   for(var y=0; y<size; y++){
     $('#board').append("<div class='row'></div>");
   };
@@ -55,53 +74,51 @@ function generateSnake(board_size){
   while (randY<2){
     randY = Math.floor(Math.random()*board.length);
   }
-  snake = {direction:'u', body: [[randX, randY], [randX, randY-1], [randX, randY-2]]};
+  // snake = {direction:'u', body: [[randX, randY], [randX, randY-1], [randX, randY-2]]};
+  snake = {direction:'u', body: [randX+'_'+randY, randX+'_'+(randY-1), randX+'_'+(randY-2)]};
+  
   $.each(snake.body, function(index,value){
-    var position = value[0]+'_'+value[1];
-    $('#'+position).addClass('snake');
+    // var position = value[0]+'_'+value[1];
+    
+    $('#'+value).addClass('snake');
   });
 
 }
 
-
-// function gameUpdate(size){
-//   move(size);
-// }
-
 function move(size){
-  console.log(snake.body);
   var tail = snake.body.pop();
-  var tail_pos = tail[0]+'_'+tail[1]
-  $('#'+tail_pos).removeClass('snake');
-  var new_head = [snake.body[0][0],snake.body[0][1]];
-  console.log(snake.body);
+  // var tail_pos = tail[0]+'_'+tail[1]
+  $('#'+tail).removeClass('snake');
 
+  var new_head = snake.body[0];
+  console.log(new_head);
+
+  var new_head_x = parseInt(new_head.split('_')[0])
+  var new_head_y = parseInt(new_head.split('_')[1])
+  console.log(new_head);
 
   switch(snake.direction){
-    case 'u': new_head[1] = new_head[1]+1; break;
-    case 'd': new_head[1] = new_head[1]-1; break;
-    case 'l': new_head[0] = new_head[0]-1; break;
-    case 'r': new_head[0] = new_head[0]+1; break;
+    case 'u': new_head_y += 1; break;
+    case 'd': new_head_y -= 1; break;
+    case 'l': new_head_x -= 1; break;
+    case 'r': new_head_x += 1; break;
   }
-  console.log(snake.body);
-
-  if (new_head[0]<0 || new_head[0]>=size || new_head[1]<0 || new_head[1]>=size){
+  new_head = new_head_x+'_'+new_head_y;
+  if (new_head_x<0 || new_head_x>=size || new_head_y<0 || new_head_y>=size || snake.body.indexOf(new_head) > -1){
     alert('You lost!');
     return game_init();
-  }else{
+  } else{
     snake.body.unshift(new_head);
-    var new_head_pos = new_head[0]+'_'+new_head[1]
-    $('#'+new_head_pos).addClass('snake');
+    $('#'+new_head).addClass('snake');
   }
 
-  if (new_head[0]+'_'+new_head[1] == fruit) {
+  if (new_head == fruit) {
     snake.body.push(tail);
-    $('#'+tail_pos).addClass('snake');
+    $('#'+tail).addClass('snake');
     $('#'+fruit).removeClass('fruit');
     generateFruit(size);
 
   }
-  console.log(snake.body);
 }
 
 
