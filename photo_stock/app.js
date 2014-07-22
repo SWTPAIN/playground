e7f267f797d9a3a196e63e2f5218146a2712e07b
KaFaiC <choikafai2004@gmail.com>
2014-07-22 10:57:30 +0800

    Add photo model

--- photo_stock/app.js
+++ photo_stock/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var photos = require('./routes/photos');
 var users = require('./routes/users');
 
 var app = express();
@@ -13,7 +15,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('photos', __dirname + '/public/photos');
+app.set('port', process.env.PORT || 3000);
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -21,7 +24,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', photos.list);
 app.use('/users', users);
 
 /// catch 404 and forward to error handler
@@ -56,4 +59,8 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+// module.exports = app;
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+})
