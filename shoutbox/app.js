f192c1a2b531aa5201b95a57cf8732fe4f193763
KaFaiC <choikafai2004@gmail.com>
2014-07-22 16:35:20 +0800

    Add validation on post by validation middleware

--- shoutbox/app.js
+++ shoutbox/app.js
@@ -15,6 +15,7 @@ var login = require('./routes/login');
 var entries = require('./routes/entries');
 var messages = require('./lib/messages');
 var user = require('./lib/middleware/user');
+var validate = require('./lib/middleware/validate');
 
 var app = express();
 
@@ -41,7 +42,9 @@ app.get('/login', login.form);
 app.post('/login', login.submit);
 app.get('/logout', login.logout);
 app.get('/post', entries.form);
-app.post('/post', entries.submit);
+app.post('/post', validate.required('entry[title]'),
+         validate.lengthAbove('entry[title]', 4),
+         entries.submit);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
